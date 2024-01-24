b771f4bb52f16572ebbe208fe9fc8e1fd0120e49
Minami127 <jws282288@gmail.com>
2024-01-24 12:45:15 +0900

    add

--- TravelApp/app/src/main/java/com/example/myapplication1/RegisterActivity.java
+++ TravelApp/app/src/main/java/com/example/myapplication1/RegisterActivity.java
@@ -1,17 +1,21 @@
 package com.example.myapplication1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.window.OnBackInvokedDispatcher;
 
 public class RegisterActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
     Button btn;
+    ImageButton imgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         progressBar = findViewById(R.id.register_progressBar);
         btn = findViewById(R.id.register_button);
+        imgBtn = findViewById(R.id.back_btn);
 
         progressBar.setVisibility(View.GONE);
 
@@ -30,10 +35,15 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-
-
-
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
+
 }
