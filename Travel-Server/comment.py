e63ffadd0a2c5f93ee906ee3a0621203f1296561
notfound404yhkim <kyh31445196@gmail.com>
2024-01-25 10:59:47 +0900

    .
    .

---
+++ Travel-Server/comment.py
@@ -0,0 +1,8 @@
+from flask import request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_restful import Resource
+from config import Config
+from mysql_connection import get_connection
+from mysql.connector import Error
+import boto3
+from datetime import datetime
