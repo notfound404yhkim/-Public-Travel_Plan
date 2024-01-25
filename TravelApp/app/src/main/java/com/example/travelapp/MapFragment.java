6771b2abbeb601fc377de4fb53088ceaedc6f012
Minami127 <151480603+Minami127@users.noreply.github.com>
2024-01-25 11:21:01 +0900

    add

--- TravelApp/app/src/main/java/com/example/travelapp/MapFragment.java
+++ TravelApp/app/src/main/java/com/example/travelapp/MapFragment.java
@@ -1,35 +1,49 @@
 package com.example.travelapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnPoiClickListener {
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
-
+    private final int FINE_PERMISSION_CODE = 1;
     private GoogleMap mMap;
-
-
-
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
+        getLastLocation();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 
@@ -43,18 +57,63 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return view;
     }
 
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    updateMap();
+                }
+            }
+        });
+    }
+
+    private void updateMap() {
+        if (mMap != null && currentLocation != null) {
+            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("내 위치"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        updateMap();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // OnPoiClickListener 등록
+        mMap.setOnPoiClickListener(this);
+    }
+
+    @Override
+    public void onPoiClick(PointOfInterest poi) {
+        // 클릭된 POI에 대한 처리 작업 수행
+        String poiName = poi.name;
+        LatLng poiLatLng = poi.latLng;
+
+        // 예를 들어, 클릭된 POI의 정보를 로그로 출력
+        Log.d("POI Clicked", "Name: " + poiName + ", Location: " + poiLatLng.toString());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(requireContext(), "위치 권한이 없습니다", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
 }
+
